6740796da1728275f603737ab5667f7b0cb790f6
mamaducks <leonad308@gmail.com>
2023-02-06 12:01:13 -0500

    added social buttons

--- src/AppBar/AppBar.jsx
+++ src/AppBar/AppBar.jsx
@@ -2,10 +2,12 @@
 import { Box, Container } from "@mui/system";
 import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material";
 
-// import CloseIcon from "@mui/icons-material/Close";
 import { ShowSmall, NoShowSmall } from "../MediaBox.jsx";
 import { AppBarButton } from "./AppBarButton.jsx";
 import logoJaya from "../jayalogo.png";
+
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 export function AppBarMedia() {
   return (
@@ -48,10 +50,7 @@ export function AppBarMedia() {
         <AppBar position="sticky" sx={{ backgroundColor: "#0d0f11" }}>
           <Toolbar>
             <Box flexGrow={1}>
-              {/* <Link href={`/`} sx={{ textDecoration: "none" }}> */}
               <Stack direction="row" gap={1}>
-                {/* <img src={leaf} alt="logo" height={40} width={40} /> */}
-
                 <Typography
                   variant="h4"
                   sx={{
@@ -65,59 +64,27 @@ export function AppBarMedia() {
                 >
                   body by jaya
                 </Typography>
-
-                {/* <Typography sx={{ alignSelf: "center" }}>by Jaya</Typography> */}
               </Stack>
-              {/* </Link> */}
             </Box>
 
             <Stack direction="row" gap={2}>
-              <Box alignSelf="center">
-                <AppBarButton />
-              </Box>
-            </Stack>
+              <a href="https://www.facebook.com/profile.php?id=100089984763471&mibextid=LQQJ4d">
+                <FacebookIcon sx={{ color: "#ffffff", fontSize: "2rem" }} />
+              </a>
 
-            {/* <div>
-                <StyledCenterStack
-                  direction="row"
-                  justifyContent="flex-end"
-                  gap={1}
-                  flexGrow={1}
-                >
-                  
-  
-                  <Avatar>
-                    <YouTubeIcon />
-                  </Avatar>
-                  <Avatar>
-                    <InstagramIcon />
-                  </Avatar>
-                  <Avatar>
-                    <PinterestIcon />
-                  </Avatar>
-                  <Avatar>
-                    <TwitterIcon />
-                  </Avatar>
-                </StyledCenterStack>
-              </div> */}
+              <a
+                href="https://www.instagram.com/bodybyjaya/?igshid=YmMyMTA2M2Y%3D"
+                className="instagram social"
+              >
+                <InstagramIcon sx={{ color: "#ffffff", fontSize: "2rem" }} />
+              </a>
+            </Stack>
           </Toolbar>
         </AppBar>
       </NoShowSmall>
 
       <Container maxWidth="md">
-        <img
-          alt=""
-          src={logoJaya}
-          width="100%"
-          height="auto"
-          // style={{
-          //   alignSelf: "center",
-          //   minWidth: 350,
-          //   // paddingBlock: "10px",
-          //   // flexGrow: 0,
-          //   // marginInline: 20,
-          // }}
-        />
+        <img alt="" src={logoJaya} width="100%" height="auto" />
       </Container>
       <NoShowSmall>
         <Stack
@@ -125,26 +92,37 @@ export function AppBarMedia() {
           justifyContent="space-evenly"
           sx={{ bgcolor: "#f4f4f4", fontFamily: "Oswald" }}
         >
-          <Button sx={{ color: "#101010", fontFamily: "Oswald" }} href="/">
+          <Button
+            size="large"
+            sx={{ color: "#101010", fontFamily: "Oswald" }}
+            href="/"
+          >
             Home
           </Button>
           <Button
+            size="large"
             sx={{ color: "#101010", fontFamily: "Oswald" }}
             href="/services"
           >
             Treatments
           </Button>
           <Button
+            size="large"
             sx={{ color: "#101010", fontFamily: "Oswald" }}
             href="/reviews"
           >
             Reviews
           </Button>
-          <Button sx={{ color: "#101010", fontFamily: "Oswald" }} href="Faq">
+          <Button
+            size="large"
+            sx={{ color: "#101010", fontFamily: "Oswald" }}
+            href="Faq"
+          >
             Faqs
           </Button>
 
           <Button
+            size="large"
             sx={{ color: "#101010", fontFamily: "Oswald" }}
             href="/contact"
           >
